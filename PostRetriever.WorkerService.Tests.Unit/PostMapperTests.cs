810bb94c2e2c09f3c24f8dc1a8f1da75ba123064
Mark Fay <phoenyx2k@outlook.com>
2023-11-28 12:12:59 -0800

    Add Post Mapper

---
+++ PostRetriever.WorkerService.Tests.Unit/PostMapperTests.cs
@@ -0,0 +1,39 @@
+﻿using PostRetriever.WorkerService.Services;
+using RedditSharp.Things;
+using AutoFixture;
+using FluentAssertions;
+
+namespace PostRetriever.WorkerService.Tests.Unit;
+
+public class PostMapperTests
+{
+    private static readonly Fixture Fixture = new();
+    private readonly Post _redditSharpPost = Fixture.Create<Post>();
+    
+    private RedditPost _result = null!;
+
+    [Fact]
+    [Trait("Category", "UnitTest")]
+    public void ShouldMapToDataType()
+    {
+        // Act
+        WhenMapIsCalled();
+
+        // Assert
+        ThenRedditPostWasReturned();
+    }
+
+    private void ThenRedditPostWasReturned()
+    {
+        _result.Id.Should().Be(_redditSharpPost.Id);
+        _result.AuthorName.Should().Be(_redditSharpPost.AuthorName);
+        _result.Upvotes.Should().Be(_redditSharpPost.Upvotes);
+        _result.Created.Should().Be(_redditSharpPost.CreatedUTC);
+    }
+
+    private void WhenMapIsCalled()
+    {
+        IMapper<Post, RedditPost> target = new PostMapper();
+        _result = target.Map(_redditSharpPost);
+    }
+}
---
+++ PostRetriever.WorkerService/Services/PostMapper.cs
@@ -0,0 +1,23 @@
+﻿using RedditSharp.Things;
+
+namespace PostRetriever.WorkerService.Services;
+
+public interface IMapper<TRedditSharp, TData>
+{
+    TData Map(TRedditSharp input);
+}
+
+public class PostMapper : IMapper<Post, RedditPost>
+{
+    public RedditPost Map(Post input)
+    {
+        var redditPost = new RedditPost();
+        
+        redditPost.Id = input.Id;
+        redditPost.Created = input.CreatedUTC;
+        redditPost.AuthorName = input.AuthorName;
+        redditPost.Upvotes = input.Upvotes;
+        
+        return redditPost;
+    }
+}
