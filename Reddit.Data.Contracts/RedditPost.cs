ac98d91e9ca70a574782573908f1c8c5990183e7
Mark Fay <phoenyx2k@outlook.com>
2023-11-28 12:12:44 -0800

    Add RedditPost to Data Contracts

---
+++ Reddit.Data.Contracts/RedditPost.cs
@@ -0,0 +1,9 @@
+﻿namespace Reddit.Data.Contracts;
+
+public class RedditPost
+{
+    public string Id { get; set; } = null!;
+    public DateTime Created { get; set; }
+    public string AuthorName { get; set; } = null!;
+    public int UpVotes { get; set; }
+}
